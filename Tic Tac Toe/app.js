6d0e576c4b3a0a07a33ff029fb86d926b09fa112
yushaabdullah <yusha814@gmail.com>
2023-05-11 16:44:46 +0600

    Optimized Tic Tac Toe added

---
+++ Tic Tac Toe/app.js
@@ -0,0 +1,93 @@
+const gameBorard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+
+const startCells = ["", "", "", "", "", "", "", "", ""];
+
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
+function createBoard() {
+  startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
+    gameBorard.append(cellElement);
+  });
+}
+
+createBoard();
+
+function addGo(e) {
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  e.target.append(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "It is now " + go + "'s turn";
+  e.target.removeEventListener("click", addGo);
+  checkScore();
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll(".square");
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let isDraw = true;
+
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins!";
+      isDraw = false;
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("cross")
+    );
+
+    if (crossWins) {
+      infoDisplay.textContent = "Cross Wins!";
+      isDraw = false;
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  if (isDraw) {
+    const occupiedSquares = document.querySelectorAll(".square > div");
+    if (occupiedSquares.length === allSquares.length) {
+      infoDisplay.textContent = "It's a draw!";
+      allSquares.forEach((square) =>
+        square.removeEventListener("click", addGo)
+      );
+    }
+  }
+}
+
+// reset game
+
+const resetBtn = document.getElementById("reset");
+
+resetBtn.addEventListener("click", function () {
+  location.reload();
+});
